42da4a5afa28d78206ce15324aa96ac02c056e13
CandeGod <Candebeatriz14@gmail.com>
2024-10-26 17:13:39 -0600

    Class for ApiRecipe grahpql queries

--- src/main/java/com/proyecto/SazonIA/controller/ApiRecipeController.java
+++ src/main/java/com/proyecto/SazonIA/controller/ApiRecipeController.java
@@ -1,50 +1,23 @@
 package com.proyecto.SazonIA.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.proyecto.SazonIA.spoonacularApi.ApiRecipe;
 import com.proyecto.SazonIA.service.ApiRecipeService;
+import com.proyecto.SazonIA.spoonacularApi.ApiRecipe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@RestController
-@Tag(name = "Api spoonacular", description = "Operations related to the spoonacular api")
+@Controller
 public class ApiRecipeController {
 
-    @Autowired
-    private ApiRecipeService apiRecipeService;
+    private final ApiRecipeService apiRecipeService;
 
-    @Operation(summary = "Get a random recipe")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Random recipe found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiRecipe.class))),
-            @ApiResponse(responseCode = "404", description = "No recipe found", content = @Content)
-    })
-    @GetMapping("/random-recipe")
-    public ResponseEntity<ApiRecipe> getRandomRecipe() {
-        ApiRecipe randomRecipe = apiRecipeService.getRandomRecipe();
-        if (randomRecipe != null) {
-            return new ResponseEntity<>(randomRecipe, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @Autowired
+    public ApiRecipeController(ApiRecipeService apiRecipeService) {
+        this.apiRecipeService = apiRecipeService;
+    }
+
+    @QueryMapping
+    public ApiRecipe getRandomRecipe() {
+        return apiRecipeService.getRandomRecipe();
     }
 }
-
-
-
-
-
-
-
-
-
--- src/main/java/com/proyecto/SazonIA/spoonacularApi/ApiRecipe.java
+++ src/main/java/com/proyecto/SazonIA/spoonacularApi/ApiRecipe.java
@@ -2,6 +2,11 @@ package com.proyecto.SazonIA.spoonacularApi;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ApiRecipe {
     
     private Integer id;
@@ -30,110 +35,28 @@ public class ApiRecipe {
 
     private ApiWinePairingRecipe winePairing;
 
-    public String getSummary() {
-        return summary;
-    }
+    
 
-    public void setSummary(String summary) {
+    public ApiRecipe(Integer id, String title, String image, Integer readyInMinutes, Integer cookingMinutes,
+            Integer preparationMinutes, String sourceName, String sourceUrl, String spoonacularSourceUrl,
+            Integer servings, List<ApiIngredientsRecipe> extendedIngredients, String summary,
+            ApiWinePairingRecipe winePairing) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.readyInMinutes = readyInMinutes;
+        this.cookingMinutes = cookingMinutes;
+        this.preparationMinutes = preparationMinutes;
+        this.sourceName = sourceName;
+        this.sourceUrl = sourceUrl;
+        this.spoonacularSourceUrl = spoonacularSourceUrl;
+        this.servings = servings;
+        this.extendedIngredients = extendedIngredients;
         this.summary = summary;
-    }
-
-    public ApiWinePairingRecipe getWinePairing() {
-        return winePairing;
-    }
-
-    public void setWinePairing(ApiWinePairingRecipe winePairing) {
         this.winePairing = winePairing;
     }
 
-    public String getSourceUrl() {
-        return sourceUrl;
-    }
-
-    public void setSourceUrl(String sourceUrl) {
-        this.sourceUrl = sourceUrl;
-    }
-
-    public String getSpoonacularSourceUrl() {
-        return spoonacularSourceUrl;
-    }
-
-    public void setSpoonacularSourceUrl(String spoonacularSourceUrl) {
-        this.spoonacularSourceUrl = spoonacularSourceUrl;
-    }
-
-    public Integer getReadyInMinutes() {
-        return readyInMinutes;
-    }
-
-    public void setReadyInMinutes(Integer readyInMinutes) {
-        this.readyInMinutes = readyInMinutes;
-    }
-
-    public Integer getCookingMinutes() {
-        return cookingMinutes;
-    }
-
-    public void setCookingMinutes(Integer cookingMinutes) {
-        this.cookingMinutes = cookingMinutes;
-    }
-
-    public Integer getPreparationMinutes() {
-        return preparationMinutes;
-    }
-
-    public void setPreparationMinutes(Integer preparationMinutes) {
-        this.preparationMinutes = preparationMinutes;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getSourceName() {
-        return sourceName;
-    }
-
-    public void setSourceName(String sourceName) {
-        this.sourceName = sourceName;
-    }
-
-    public Integer getServings() {
-        return servings;
-    }
-
-    public void setServings(Integer servings) {
-        this.servings = servings;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public List<ApiIngredientsRecipe> getExtendedIngredients() {
-        return extendedIngredients;
-    }
-
-    public void setExtendedIngredients(List<ApiIngredientsRecipe> extendedIngredients) {
-        this.extendedIngredients = extendedIngredients;
-    }
-
+    
     
 }
 
